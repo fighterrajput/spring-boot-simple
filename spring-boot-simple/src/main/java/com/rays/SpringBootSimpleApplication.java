dd8e32c414921c9fc1e859bdb593c83ca5c91d90
fighterrajput <ankitsinghtushar@gmail.com>
2024-10-05 14:00:29 +0530

    done

--- spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,7 +28,10 @@ public class SpringBootSimpleApplication {
 		WebMvcConfigurer w = new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				CorsRegistration cors = registry.addMapping("/**");
+				cors.allowedOrigins("http://localhost:4200");
+				cors.allowedHeaders("*");
+				cors.allowCredentials(true);
 			}
 
 			public void addInterceptors(InterceptorRegistry registry) {
